a089dbdfce95a6844b2516ee9f475c193bd07a37
Vanessa Mathebula <nvmabophe@gmail.com>
2024-11-20 09:19:53 +0200

    cleaned test script

--- tests/testthat/test_number_to_words.R
+++ tests/testthat/test_number_to_words.R
@@ -1,13 +1,10 @@
 
-
 # *******************************
 # Setup
 # *******************************
 
 
 pacman::p_load(dplyr, tibble, stringr, testthat, roxygen2, rmarkdown, janitor, readr, here)
-
-
 
 
 # *******************************
@@ -19,13 +16,14 @@ test_that("the numbers are correctly converted", {
 
   source(here("R", "number_to_words.R"))
 
-  #file_path = "../data/unit_test_data.txt"
   file_path <- here("data", "unit_test_data.txt")
 
+  # Convert numerical data dynamically using created function
   dynamically_converted_tibble <-
     number_to_words(file_path = file_path) %>%
     arrange(input)
 
+  # Load the pre-manually converted numerical data
   imported_manually_converted_tibble <-
     read_delim(file = file_path, delim = "\t") %>% # This places the loaded data in columns
     as_tibble() %>%
@@ -33,10 +31,9 @@ test_that("the numbers are correctly converted", {
     select(input = "test_input", output = "expected_output") %>%
     arrange(input)
 
-  # Check if tibble1 and tibble2 are identical
+  # Check if manually converted tibble is the same as the dynamically converted
 
   expect_true(all.equal(dynamically_converted_tibble, imported_manually_converted_tibble) == TRUE) # This should pass
-
 
 
 })
